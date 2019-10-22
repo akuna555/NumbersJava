4f1eac6283dfba2e797eee770e10aa24e72e7280
Oksana <oksanakulba35@gmail.com>
2019-10-22 17:29:49 -0500

    Second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello  World and all people");
+        System.out.println("Good Evening and Day");
     }
 }
