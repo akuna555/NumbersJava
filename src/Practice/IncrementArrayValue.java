49097f30d7a372b747b8b77c8b638038393b45ab
Oksana <oksanakulba35@gmail.com>
2019-11-18 20:53:22 -0600

    Add november 18

---
+++ src/Practice/IncrementArrayValue.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class IncrementArrayValue {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8,};
+        char ch =',';
+        for(int num: nums){
+            System.out.print(num+ " ");
+
+        }
+        System.out.println();
+    }
+}
